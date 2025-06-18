ddfd59c651799bc83dc9444dd01258b184a78eee
naufalfzml <naufalfzml07@gmail.com>
2025-06-18 12:42:20 +0700

    LAASTTT DONEEE

--- src/app/components/FormPendaftaranTP.tsx
+++ src/app/components/FormPendaftaranTP.tsx
@@ -236,9 +236,6 @@ function FormPendaftaran() {
               viewBox="0 0 24 24"
             >
               <path
-                strokeLinecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
                 d="M21 13.255A23.931 23.931 0 0112 15c-3.183 0-6.22-.62-9-1.745M16 6V4a2 2 0 00-2-2h-4a2 2 0 00-2 2v2m4 6h.01M5 20h14a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
               ></path>
             </svg>
@@ -446,9 +443,6 @@ function FormPendaftaran() {
                       viewBox="0 0 24 24"
                     >
                       <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        stroke-width="2"
                         d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
                       ></path>
                     </svg>
